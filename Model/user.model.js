edf3b6a94ce37671d1de1b66e9e0613b7b504ee2
Uzair <uzairmd073@gmail.com>
2023-05-10 22:43:55 +0530

    sum fix

--- Model/user.model.js
+++ Model/user.model.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 
-const UserSchema = new mongoose.Schema({
-    "Email": String,
-    "Password":String,
-    "Confirm_Password": String,
+const userSchema=mongoose.Schema({
+    name : String,
+email :String,
+gender : String,
+password : String,
+age : Number,
+city : String,
+is_married : Boolean,
 })
 
-const Usermodel = mongoose.model("usermockdata", UserSchema)
 
-module.exports = {
-    Usermodel
+const userModel=mongoose.model("userdataeval",userSchema)
+
+
+module.exports={
+    userModel
 }
-
--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,64 +1,64 @@
-const express = require('express')
+const express = require("express")
 const userrouter = express.Router()
-const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
-const { Usermodel } = require('../Model/user.model');
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+const { Usermodel } = require("../Model/user.model");
 
 userrouter.get("/",(req,res)=>{
 res.end("hello")
 })
 
-// userrouter.post("/signup", async (req, res) => {
+userrouter.post("/signup", async (req, res) => {
 
-//     const { Email,Password,Confirm_Password}=req.body
+    const { Email,Password,Confirm_Password}=req.body
 
-//     try {
-//         let already_exist = await Usermodel.find({Email})
-//         if(already_exist.length){
-//             res.status(500).send({"msg":"User Already exist"})
-//         }else{
-//             bcrypt.hash(Password, 5, async function(err, hash) {
-//                let data = new Usermodel({Email,Password:hash,Confirm_Password:hash})
-//                await data.save()
-//                 res.status(200).send({"msg":"User Successfully Registered"})
+    try {
+        let already_exist = await Usermodel.find({Email})
+        if(already_exist.length){
+            res.status(500).send({"msg":"User Already exist"})
+        }else{
+            bcrypt.hash(Password, 5, async function(err, hash) {
+               let data = new Usermodel({Email,Password:hash,Confirm_Password:hash})
+               await data.save()
+                res.status(200).send({"msg":"User Successfully Registered"})
               
-//             });
+            });
 
-//         }
+        }
         
 
-//     } catch (error) {
-//         res.status(500).send({"msg":error.message})
-//     }
+    } catch (error) {
+        res.status(500).send({"msg":error.message})
+    }
 
 
 
  
-// })
+})
 
 
-// userrouter.post("/login", async (req, res) => {
-// const {Email,Password}=req.body
-// try {
-//     let findemail = await Usermodel.find({Email})
-//     let pass= findemail[0].Password
+userrouter.post("/login", async (req, res) => {
+const {Email,Password}=req.body
+try {
+    let findemail = await Usermodel.find({Email})
+    let pass= findemail[0].Password
  
-//         bcrypt.compare(Password, pass, function(err, result) {
-//             if(result){
-//              res.status(200).send({"msg":"User Successfully loggedIn",token:jwt.sign({ userId:findemail[0]._id }, 'user')})
-//             }else{
-//                 res.status(500).send({"msg":err.message})
-//             }
-//          });
+        bcrypt.compare(Password, pass, function(err, result) {
+            if(result){
+             res.status(200).send({"msg":"User Successfully loggedIn",token:jwt.sign({ userId:findemail[0]._id }, 'user')})
+            }else{
+                res.status(500).send({"msg":err.message})
+            }
+         });
      
     
 
-// } catch (error) {
-//     res.status(500).send({"msg":error.message})
-// }
+} catch (error) {
+    res.status(500).send({"msg":error.message})
+}
 
    
-// })
+})
 
 module.exports = {
     userrouter
